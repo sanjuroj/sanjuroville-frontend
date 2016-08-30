74c13f6a892b57a614ee8a1520aee159d8e0407a
Sanjuro Jogdeo <weisenheimer@sanjuro.net>
2016-08-30 12:58:04 -0700

    Add paletton file and webpack live file

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var path = require('path');
+var BundleTracker = require('webpack-bundle-tracker');
+
+module.exports = {
+    entry: "./main.js",
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "bundle.js",
+        publicPath: "/build/",
+        headers: { 'Access-Control-Allow-Origin': '.sanjuro.net' } //eliminate in live config
+        
+
+    },
+
+    plugins: [
+        new BundleTracker({filename: './webpack-stats.json'})
+    ],
+
+    resolve: {
+        alias: {
+            contactImg: path.resolve(__dirname, 'assets/icons')
+        }
+    },
+
+
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" },
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+            { test: /\.(jpg|svg|png)$/, exclude: /node_modules/, loader: "url-loader" }
+        ]
+    }
+
+    
+};
+
+
