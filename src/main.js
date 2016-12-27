ad87300de6501fc4f1664e3c9a1a68d1998f6f00
Sanjuro Jogdeo <weisenheimer@sanjuro.net>
2016-12-27 10:25:58 -0800

    -Changed routing so navbar is added to the appropriate pages, but not to old-webpage. -Added logo fonts -tinkered with overall page css

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import getRData from './reducers';
 import thunk from 'redux-thunk';
-import { browserHistory } from 'react-router';
 import routes from './routes';
 
 
@@ -22,7 +21,7 @@ let store = createStore(
 
 ReactDOM.render(
     <Provider store={store}>
-        {routes(browserHistory)}
+        {routes()}
     </Provider>,
     document.getElementById('target')
 );
--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,32 @@
-import React from 'react';
-import { Router, Route} from 'react-router';
+import React, { Component } from 'react';
+import { Router, Route, IndexRoute, browserHistory} from 'react-router';
 import Resume from './containers/Resume';
 import Home from './components/Home';
 import About from './components/About';
 import OldHomepage from './components/OldHomepage';
+import NavBar from './components/NavBar';
 
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <NavBar />
+                {this.props.children}
+            </div>
+
+        )
+    }
+}
 
 export default function routes(history){
     return (
-        <Router history={history}>
-            <Route path='/' component={Home} />
-            <Route path='home' component={Home} />
-            <Route path='resume' component={Resume} />
-            <Route path='about' component={About} />
+        <Router history={browserHistory}>
+            <Route path='/' component={App}>
+                <IndexRoute component={Home} />
+                <Route path='home' component={Home} />
+                <Route path='resume' component={Resume} />
+                <Route path='about' component={About} />
+            </Route>
             <Route path='old-homepage' component={OldHomepage} />                
         </Router>
     );
